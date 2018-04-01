b72eb54efd8f8f059cb1370d9302244804b0b5df
gnarmedia <gnarmedia@users.noreply.github.com>
2018-04-01 04:57:11 -0700

    refactor(TaskList): Break Task out of TaskList component
    This breaks Task out of the TaskList component to simplify each
    component,  make things reusable, and allow this component to be tested.

---
+++ src/components/Task.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+// import components
+import Task from "./Task.jsx";
+
+// load some fake tasks
+const task = {
+    id: 1,
+    title: "Task 1",
+    isComplete: false
+};
+
+describe("Task", function() {
+    it("renders the Task with completed status", () => {
+        const tree = renderer
+            .create(<Task title="Complete Task" isComplete={true} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it("renders the Task with incomplete status", () => {
+        const tree = renderer
+            .create(<Task title="Incomplete task" isComplete={false} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
